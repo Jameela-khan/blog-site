4cc8e07dffd78894f60ca128068370c974a4c179
Jameela <jameelakhan52@gmail.com>
2025-09-12 22:44:30 +0530

    connect db with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/posts')
+      .then(res => setPosts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-         Hello App    
-        </p>
-        
-      </header>
+    <div>
+      <h1>Blog Posts</h1>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
